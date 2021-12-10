7d38dbcdc463cd1232dc3bf60ef8ad9c96eb8f11
Gh05t-1337 <95411780+Gh05t-1337@users.noreply.github.com>
2021-12-10 23:18:44 +0100

    Update XOR_Images.py

--- 2/XOR_Images.py
+++ 2/XOR_Images.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from PIL import Image, ImageChops, ImageOps
+from PIL import Image, ImageChops
 im1 = Image.open('image.png').convert("1")
 im2 = Image.open('image2.png').convert("1")
 
