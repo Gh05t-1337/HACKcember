5d4c2514e55c55cee1631c686ea5a256745fbf8b
Gh05t-1337 <95411780+Gh05t-1337@users.noreply.github.com>
2021-12-10 23:08:40 +0100

    Update XOR_Images.py

--- 2/XOR_Images.py
+++ 2/XOR_Images.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import numpy as np
 from PIL import Image, ImageChops, ImageOps
-im2 = ImageOps.grayscale(Image.open('image.png')).convert("1")
-im1 = ImageOps.grayscale(Image.open('image2.png')).convert("1")
+im1 = Image.open('image.png').convert("1")
+im2 = Image.open('image2.png').convert("1")
 
 result = ImageChops.logical_xor(im1,im2)
 result.show()
