4bf6d138f9cf93a73d5a21d21bc2e2e4793b220f
Gh05t-1337 <95411780+Gh05t-1337@users.noreply.github.com>
2021-12-13 07:07:58 +0100

    Update XOR_Images.py

--- 2/XOR_Images.py
+++ 2/XOR_Images.py
@@ -4,6 +4,9 @@ im1 = Image.open('image.png').convert("1")
 im2 = Image.open('image2.png').convert("1")
 
 result = ImageChops.logical_xor(im1,im2)
-result.show()
 result.save('result.png')
+
+#aus einer enfernung von ca. 30cm oder mehr, klappt das einscannen des qr codes relativ gut.
+#falls der qr code trotzdem nicht richtig erkannt wird, 
+#einfach mit Paint oder ähnlichen Professionellen Tools nachbearbeiten
 
