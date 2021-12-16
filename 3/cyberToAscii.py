9a7be45b8963ac96e801bcf837c4d7c3619edba0
Gh05t-1337 <95411780+Gh05t-1337@users.noreply.github.com>
2021-12-16 02:37:29 +0100

    Create cyberToAscii.py

---
+++ 3/cyberToAscii.py
@@ -0,0 +1,22 @@
+
+with open("cyber.txt") as f:
+    cyber=f.read().lower()
+
+cyberliste=""
+
+for wort in cyber.split():
+    if "cyber" in wort:
+        if wort.split("cyber")[1][0]=="-":
+            cyberliste+="1"
+        else:
+            cyberliste+="0"
+
+
+print(cyberliste+"\n") #binary
+
+hexdec='%x' % int(cyberliste, 2) #spaßeshalber hexadecimal
+print(hexdec+"\n")
+
+#zu ascii konvertieren (geht sicher auch unkomplizierter bzw. leserlicher...)
+text=''.join(chr(int(cyberliste[i:i+8], 2)) for i in range(0, len(cyberliste), 8))
+print(f"Passwort: {text}"+"\n")
