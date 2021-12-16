2e702bf32b9f6fe6ab95492a35831daa78eb853d
Gh05t-1337 <95411780+Gh05t-1337@users.noreply.github.com>
2021-12-16 02:46:54 +0100

    Update cyberToAscii.py

--- 3/cyberToAscii.py
+++ 3/cyberToAscii.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 #link zur Challenge: https://www.floriandalwigk.de/das-cyber-rotkaeppchen/
 with open("cyber.txt") as f:
-    cyber=f.read().lower()
+    cyber=f.read().lower()  #Cybermärchen lesen
 
 cyberliste=""
 
 for wort in cyber.split():
-    if "cyber" in wort:
-        if wort.split("cyber")[1][0]=="-":
+    if "cyber" in wort:     #für alle Cyber Wörter:
+        if wort.split("cyber")[1][0]=="-":  #falls mit Bindestrich getrennt: 1, sonst: 0
             cyberliste+="1"
         else:
             cyberliste+="0"
