56d9af56a501e890ebf2c09443703f145cba4c2f
Gh05t-1337 <95411780+Gh05t-1337@users.noreply.github.com>
2021-12-22 21:25:39 +0100

    Update findPossible.py

--- 4/findPossible.py
+++ 4/findPossible.py
@@ -2,6 +2,9 @@
 #link zur Challenge: https://www.floriandalwigk.de/das-letzte-geschenk-hackcember-2021/
 #findet die möglichen passwörter. (müssen dann einfach nacheinander an der zip getestet werden)
 #es ist ein passwort in der rockyou.txt, das 24*4=96 zeichen hat.
+
+#rockyou.txt wird benötigt. einfach danach googlen und downloaden
+
 text=""
 with open("rockyou.txt","r",encoding='latin-1') as f:
 	text=f.read()
